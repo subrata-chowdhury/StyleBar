42fab299d3af8c92a4d6cc204951a9829f2c4351
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2024-02-28 23:19:46 +0530

    added some responsiveness for mobile

--- Script/main.js
+++ Script/main.js
@@ -1,44 +1,83 @@
-const epicGameBtnCon = document.querySelector(".epic-games-btn-container");
-const logoBtnPopup = document.querySelector(".logo-btn-popup");
+function epicGamePopUpToggler() {
+    const epicGameBtnCon = document.querySelector(".epic-games-btn-container");
+    const logoBtnPopup = document.querySelector(".logo-btn-popup");
 
-epicGameBtnCon.addEventListener("click", () => {
-    if (document.querySelector(".epic-games-btn-container.active")) {
+    epicGameBtnCon.addEventListener("click", () => {
+        if (document.querySelector(".epic-games-btn-container.active")) {
+            try {
+                epicGameBtnCon.classList.remove("active");
+                logoBtnPopup.classList.remove("active");
+            } catch (error) { }
+        } else {
+            try {
+                epicGameBtnCon.classList.add("active");
+                logoBtnPopup.classList.add("active");
+            } catch (err) { }
+        }
+    })
+
+
+    // Closeing epicGamePopUp when menu is hoverd
+    document.querySelector(".menu").addEventListener("mousemove", () => {
         try {
             epicGameBtnCon.classList.remove("active");
             logoBtnPopup.classList.remove("active");
-        } catch (error) {
+        } catch (err) { }
+    })
+}
+epicGamePopUpToggler();
 
-        }
-    } else {
+
+function searchContainerToggler() {
+    const searchContainer = document.querySelector(".search-container");
+
+    document.querySelector(".search-container .search-icon").addEventListener("click", () => {
         try {
-            epicGameBtnCon.classList.add("active");
-            logoBtnPopup.classList.add("active");
-        } catch (err) {
+            searchContainer.classList.add("active");
+        } catch (err) { }
+    })
 
-        }
-    }
+    document.querySelector(".search-container .cross-icon").addEventListener("click", () => {
+        try {
+            searchContainer.classList.remove("active");
+        } catch (err) { }
+    })
+}
+searchContainerToggler();
+
+function menubarTogglerForMobile() {
+    const threeLineMenu = document.querySelector(".three-line-menu");
+    const closeMenuBarBtn = document.querySelector(".cross-for-close-menu");
+    const menubar = document.querySelector(".menubar-container");
+
+    closeMenuBarBtn.addEventListener("click", () => {
+        try {
+            menubar.classList.remove("active");
+            closeMenuBarBtn.classList.remove("active");
+            threeLineMenu.classList.add("active");
+        } catch (err) { }
+    })
+
+    threeLineMenu.addEventListener("click", () => {
+        try {
+            menubar.classList.add("active");
+            closeMenuBarBtn.classList.add("active");
+            threeLineMenu.classList.remove("active");
+        } catch (error) { }
+    })
+}
+menubarTogglerForMobile();
+
+document.querySelectorAll(".menu").forEach((e) => {
+    e.addEventListener("click", (event) => {
+        if(window.innerWidth > 1270) return;
+        try {
+            document.querySelector(".menu.active").classList.remove("active");
+        } catch (err) { }
+        e.classList.add("active")
+    })
 })
 
-document.querySelector(".menu").addEventListener("mousemove", () => {
-    try {
-        epicGameBtnCon.classList.remove("active");
-        logoBtnPopup.classList.remove("active");
-    } catch (err) {
-
-    }
-})
-
-document.querySelector(".search-container .search-icon").addEventListener("click", () => {
-    try {
-        document.querySelector(".search-container").classList.add("active");
-    } catch (err) { }
-})
-
-document.querySelector(".search-container .cross-icon").addEventListener("click", () => {
-    try {
-        document.querySelector(".search-container").classList.remove("active");
-    } catch (err) { }
-})
 
 // window.addEventListener("resize",()=>{
 //     changeImagesBasedOnWidth();
